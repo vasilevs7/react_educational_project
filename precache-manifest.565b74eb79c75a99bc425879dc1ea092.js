1f7c615cbb580a2c67bf5f1ee1a7cd0fd634a466
Vasilev <vasilevs713@gmail.com>
2019-10-15 03:15:08 +0300

    Updates

---
+++ precache-manifest.565b74eb79c75a99bc425879dc1ea092.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56afe76f2396243afce40be653981bdd",
+    "url": "/react-educational-project/index.html"
+  },
+  {
+    "revision": "25b210b04d7c583e4640",
+    "url": "/react-educational-project/static/css/2.11dba33f.chunk.css"
+  },
+  {
+    "revision": "8b54b5d114a32d2c90b2",
+    "url": "/react-educational-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "25b210b04d7c583e4640",
+    "url": "/react-educational-project/static/js/2.628f008a.chunk.js"
+  },
+  {
+    "revision": "8b54b5d114a32d2c90b2",
+    "url": "/react-educational-project/static/js/main.ae12b42e.chunk.js"
+  },
+  {
+    "revision": "e29712ef8544dda43f38",
+    "url": "/react-educational-project/static/js/runtime-main.3f75e359.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-educational-project/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-educational-project/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-educational-project/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-educational-project/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-educational-project/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
